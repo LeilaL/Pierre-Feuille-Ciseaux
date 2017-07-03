7f67513eebe7d36ccbc47ece68e8421036df4052
Leila.L <leila.lababsa@hotmail.fr>
2017-07-03 14:22:56 +0200

    main.js: finish random; determinate number for each items and show choices

--- js/main.js
+++ js/main.js
@@ -7,3 +7,16 @@ var pointUser = 0;
   while (pointUser < 3 && pointComputer <  3) {
   user = prompt("Pierre, feuille ou ciseaux");
   computer = Math.floor((Math.random() * 3) + 1);
+
+  if (computer === 1) {
+    computer="pierre";
+}
+else if (computer === 2) {
+    computer="feuille";
+}
+else {
+    computer="ciseaux";
+}
+
+console.log("Vous: " + user);
+console.log("l'Ordinateur: " + computer);
